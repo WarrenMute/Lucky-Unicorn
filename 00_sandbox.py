8e16c03911e1e047a1a6bfaa04f346f211d6c912
WarrenMute <99296450+WarrenMute@users.noreply.github.com>
2022-02-16 11:47:27 +1300

    deleted number experiment

--- 00_sandbox.py
+++ 00_sandbox.py
@@ -1,16 +1,3 @@
-# Ask users for a number
-
-Get_number = int(input("Choose a number? "))
-# Multiply the number by 5
-times_five = Get_number * 5
-
-answer = "{} times five is equal to " \
-         "{}".format(Get_number, times_five)
-
-# Output the result
-print(answer)
-
-
 # Get name until an exit code is entered...
 
 name = ""
